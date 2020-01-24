d76e7b85af5fc94e82b8cb2582023e9b861f7ae6
Adam Jurík <ja004@seznam.cz>
2020-01-24 16:31:00 +0100

    Obstackles

---
+++ Assets/VrTry/Scripts/ObstacleManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleManager : MonoBehaviour
+{
+	[SerializeField]
+	List<GameObject> toActivate;
+
+	private void Start()
+	{
+		SetAllChildrenActive(transform);
+	}
+
+	private void SetAllChildrenActive(Transform pTransform)
+	{
+		if(pTransform.childCount == 0)
+		{
+			pTransform.gameObject.SetActive(true);
+			return;
+		}
+
+		foreach(Transform child in pTransform)
+		{
+			SetAllChildrenActive(child);
+		}
+	}
+}
